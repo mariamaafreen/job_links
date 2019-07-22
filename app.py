583a88922beb8354e0e1f4e497d9c66aa657991c
Mariam <sanubia99@gmail.com>
2019-07-22 15:34:57 +0530

    app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import os
 from models.schema import *
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
